5dbc6980f36846778f4bc4e79cdf4de271fe65b8
Edward <kabueedward@yahoo.com>
2014-03-03 10:55:31 +0300

    My first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Batchtwo::Application.config.secret_key_base = '3a751805d9047202d8093d06550e7d2779960e5a84a6cafa41ea874eb3a68dcc8b237f59814a882c260715bf7971bd88932ba359316397cf9b54ac67125e6f2c'
